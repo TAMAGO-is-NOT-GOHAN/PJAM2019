a5fc97f57893daf0ccf68e8f50d6c537251da00e
itok01 <itok01tech@gmail.com>
2019-11-30 15:39:08 +0900

    _

--- main.go
+++ main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"strconv"
 
+	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
 )
 
+type User struct {
+	Name  string `json:"name"`
+	Score int    `json:"score"`
+}
+
 func main() {
 	router := gin.Default()
 
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: projectID}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
 	router.GET("/", func(c *gin.Context) {
+		user User
+		user.Name := c.Query("name")
+		user.Score, _ := strconv.Atoi(c.Query("score"))
+
+		_, err := client.Collection("tng-pjam2019").Doc("ranking").Set(ctx, map[string]interface{}{
+			"name":    user.Name,
+			"score":   user.Score,
+		})
+		if err != nil {
+			log.Printf("An error has occurred: %s", err)
+		}
+
 		c.String(http.StatusOK, "Hello World!")
 	})
 
