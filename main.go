7643054895b4dd41badcb330961d331b91587407
higashi000 <higashi.ict@gmail.com>
2021-02-13 23:14:24 +0900

    Fix indent

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"context"
-//	"os/user"
-  "strconv"
+	//	"os/user"
+	"fmt"
 	"log"
-  "fmt"
 	"net/http"
+	"strconv"
+
+	"sort"
 
 	firebase "firebase.google.com/go"
-  "google.golang.org/api/iterator"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/iterator"
 	"google.golang.org/appengine"
-  "sort"
 )
 
 type User struct {
@@ -21,11 +22,11 @@ type User struct {
 }
 
 type Ranking struct {
-  Rank1 User `json:"rank1"`
-  Rank2 User `json:"rank2"`
-  Rank3 User `json:"rank3"`
-  Rank4 User `json:"rank4"`
-  Rank5 User `json:"rank5"`
+	Rank1 User `json:"rank1"`
+	Rank2 User `json:"rank2"`
+	Rank3 User `json:"rank3"`
+	Rank4 User `json:"rank4"`
+	Rank5 User `json:"rank5"`
 }
 
 func main() {
@@ -45,43 +46,43 @@ func main() {
 	defer client.Close()
 
 	router.GET("/getRanking/:number", func(c *gin.Context) {
-    iter := client.Collection("user").Documents(ctx)
-    var resultData []User
+		iter := client.Collection("user").Documents(ctx)
+		var resultData []User
 
-    number := c.Params.ByName("number")
+		number := c.Params.ByName("number")
 
-    num, _ := strconv.Atoi(number)
+		num, _ := strconv.Atoi(number)
 
-    for {
-      doc, err := iter.Next()
-      var tmp User
-      if err == iterator.Done {
-        break
-      }
-      if err != nil {
-        return
-      }
-      fmt.Println(doc.Data())
-      doc.DataTo(&tmp)
-      resultData = append(resultData, tmp)
-    }
+		for {
+			doc, err := iter.Next()
+			var tmp User
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return
+			}
+			fmt.Println(doc.Data())
+			doc.DataTo(&tmp)
+			resultData = append(resultData, tmp)
+		}
 
-    sort.Slice(resultData, func(i, j int) bool {
-      return resultData[i].Score > resultData[j].Score
-    })
+		sort.Slice(resultData, func(i, j int) bool {
+			return resultData[i].Score > resultData[j].Score
+		})
 
-    var res string
+		var res string
 
-    tmpNum := strconv.Itoa(resultData[num - 1].Score)
+		tmpNum := strconv.Itoa(resultData[num-1].Score)
 
-    res = number + "位" + " " + resultData[num - 1].Name + " " + tmpNum
+		res = number + "位" + " " + resultData[num-1].Name + " " + tmpNum
 
-    c.String(200, res)
+		c.String(200, res)
 	})
 
 	router.POST("/postResult", func(c *gin.Context) {
-    var user User
-    c.BindJSON(&user)
+		var user User
+		c.BindJSON(&user)
 
 		_, _, err := client.Collection("user").Add(ctx, map[string]interface{}{
 			"name":  user.Name,
